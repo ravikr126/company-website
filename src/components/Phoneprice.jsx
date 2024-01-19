6ca3d981ae2cd075c930419557bddddc0a167ab4
Ravi Kumar <ravi14577@gmail.com>
2024-01-19 17:56:32 +0530

    phone price section done

--- src/components/Phoneprice.jsx
+++ src/components/Phoneprice.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Phoneprice = () => {
   return (
     <>
-      <div className="mx-5 h-96 p-5 bg-[url('https://images.pexels.com/photos/3006340/pexels-photo-3006340.jpeg?auto=compress&cs=tinysrgb&w=600')]">
-        <p>Phone Prices</p>
+      <div className="rounded-xl mx-5 h-96 p-5 bg-[url('https://images.pexels.com/photos/3006340/pexels-photo-3006340.jpeg?auto=compress&cs=tinysrgb&w=600')]">
+        <p className="text-white text-4xl font-bold">Phone Prices</p>
 
-        <div>
-          <p>Search for a Phone</p>
-          <p>
+        <div className="drop-shadow-xl p-5 mt-8 border rounded-xl h-[250px] w-1/2 bg-[url('https://images.pexels.com/photos/531767/pexels-photo-531767.jpeg?auto=compress&cs=tinysrgb&w=600')]">
+          <p className="text-3xl font-bold text-white mb-2" >Search for a Phone</p>
+          <p className="text-base font-bold text-white">
             Select a year , Brands and models to see what you cann expect to pay
             for it this week.
           </p>
