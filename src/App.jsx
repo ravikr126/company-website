84464362dd241731834df21a2523061c4fb103bf
Ravi Kumar <ravi14577@gmail.com>
2024-01-19 17:57:48 +0530

    phone +shop by brand

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header1 from './components/Header1'
 import Header2 from './components/Header2';
 import Hero1 from './components/Hero1'
 import Phoneprice from './components/Phoneprice';
+import ShopbyBrand from './components/ShopbyBrand';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
    <Header2/>
    <Hero1/>
    <Phoneprice/>
+   <ShopbyBrand/>
    </>
   );
 }
---
+++ src/components/ShopbyBrand.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShopbyBrand = () => {
+  return (
+    <div>ShopbyBrand</div>
+  )
+}
+
+export default ShopbyBrand
