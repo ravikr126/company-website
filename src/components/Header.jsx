17ab962365ee47e4ec5296be5621141cb3833317
Ravi Kumar <ravi14577@gmail.com>
2024-01-19 04:42:42 +0000

    start working on v2 of company website

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <>
+   <div>
+    <div>
+        <input type="text" placeholder='' />
+    </div>
+   </div>
+   
+   
+   
+   </>
+  )
+}
+
+export default Header
