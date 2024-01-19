f1c4ec0fde88d97221f82432efe138a1cce3fd45
Ravi Kumar <ravi14577@gmail.com>
2024-01-19 17:26:15 +0530

    image fix in hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Header1 from './components/Header1'
 import Header2 from './components/Header2';
 import Hero1 from './components/Hero1'
+import Phoneprice from './components/Phoneprice';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
    <Header1/>
    <Header2/>
    <Hero1/>
+   <Phoneprice/>
    </>
   );
 }
---
+++ src/components/Phoneprice.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Phoneprice = () => {
+  return (
+    <>
+      <div className="mx-5 h-96 p-5 bg-[url('https://images.pexels.com/photos/3006340/pexels-photo-3006340.jpeg?auto=compress&cs=tinysrgb&w=600')]">
+        <p>Phone Prices</p>
+
+        <div>
+          <p>Search for a Phone</p>
+          <p>
+            Select a year , Brands and models to see what you cann expect to pay
+            for it this week.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Phoneprice;
