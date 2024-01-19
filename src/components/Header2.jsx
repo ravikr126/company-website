af8460be04737cad4c95774326725d8abefa119d
Ravi Kumar <ravi14577@gmail.com>
2024-01-19 14:25:25 +0530

    all alignment fix

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -1,5 +1,7 @@
 // Header.js
 import React from "react";
+import { FaRegUser } from "react-icons/fa";
+import { FiShoppingBag } from "react-icons/fi";
 
 const Header2 = () => {
   const links = [
@@ -10,15 +12,19 @@ const Header2 = () => {
   ];
 
   const rightOptions = [
-    { id: 5, label: "Sign In", href: "/signin" },
-    { id: 6, label: "Sign Out", href: "/signout" },
+    {
+      id: 5,
+      icon: <FaRegUser />,
+      label: "Sign In",
+      href: "/signin",
+    },
+    { id: 6, icon: <FiShoppingBag /> , label: "Cart", href: "/cart" },
   ];
 
   return (
     <header className="bg-blue-500 text-white py-4">
-      
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
-      logo
+        <div className="ml-20">logo</div>
         {/* Centered Links */}
         <nav className="ml-50 flex items-center justify-center gap-20 md:justify-start">
           {links.map((link) => (
@@ -33,14 +39,15 @@ const Header2 = () => {
         </nav>
 
         {/* Right Side Options */}
-        <div className="flex mt-2 md:mt-0">
+        <div className="flex mt-2 md:mt-0 mr-5">
           {rightOptions.map((option) => (
             <a
               key={option.id}
               href={option.href}
-              className="text-lg mx-4 hover:underline"
+              className="text-lg mx-4 hover:underline flex items-center"
             >
-              {option.label}
+              {option.icon} {/* Render the icon directly */}
+              <span className="ml-2">{option.label}</span>
             </a>
           ))}
         </div>
