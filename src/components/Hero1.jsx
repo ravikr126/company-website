a060b65d673aebd89dd38a285952c11ff7b5e21a
Ravi Kumar <ravi14577@gmail.com>
2024-01-23 16:58:06 +0530

    hear1 fix

--- src/components/Hero1.jsx
+++ src/components/Hero1.jsx
@@ -52,87 +52,82 @@ function Hero1() {
 
   return (
     <div className="flex flex-col md:flex-row p-5 gap-10">
-      {/* 1st slider */}
+    {/* 1st slider */}
+    <div className="md:w-1/2 w-full h-[400px] relative group overflow-hidden">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      <div className="absolute top-0 left-0 md:top-4 md:left-4 p-4 text-[#756AB6]">
+        <h2 className="text-base font-bold mb-2">Android</h2>
+        <h1 className="text-4xl mb-3">OPPO</h1>
+        <p className="text-4xl mb-4 hidden md:block font-bold">
+          OPPO  Reno 10 
+        </p>
+        <p className="text-4xl mb-4 hidden md:block font-bold">Pro 5G</p>
+        <p>The Portrait </p>
+        <p>Expert</p>
+        <button
+          className="bg-[#FFA732] text-white font-bold px-8 py-2 rounded mt-5"
+          onClick={nextSlide}
+        >
+          Shop Now
+        </button>
+      </div>
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className={`w-6 h-1 bg-gray-300 rounded-full transition duration-300 ${
+              slideIndex === currentIndex ? "bg-blue-500" : ""
+            }`}
+          ></div>
+        ))}
+      </div>
+      
+     
+      <div className="hidden md:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+    </div>
 
-      <div className="md:w-1/2 w-full h-[400px] relative group overflow-hidden">
-        {/* Image */}
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-        ></div>
-
-        {/* Left Arrow */}
-        <div className="hidden md:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactLeft onClick={prevSlide} size={30} />
-        </div>
-
-        {/* Right Arrow */}
-        <div className="hidden md:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactRight onClick={nextSlide} size={30} />
-        </div>
-
-        {/* Dot navigation */}
-        <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-          {slides.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className={`w-6 h-1 bg-gray-300 rounded-full transition duration-300 ${
-                slideIndex === currentIndex ? "bg-blue-500" : ""
-              }`}
-            ></div>
-          ))}
-        </div>
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-center md:text-left text-white">
-          <h2 className="text-3xl font-bold mb-2">Your Slide Title</h2>
-          <p className="text-lg mb-4">Your slide description goes here.</p>
-
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-full"
-            onClick={nextSlide}
-          >
-            Next
+    {/* 2nd div */}
+    <div className="md:w-1/2 w-full flex flex-col gap-2 relative">
+      <div className="flex-grow h-[180px] relative">
+        <img
+          src="https://images.pexels.com/photos/56904/pexels-photo-56904.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          className="w-full h-full rounded-2xl object-cover"
+        />
+        <div className="absolute top-3 left-0 p-4 text-white mx-5 ">
+          <p className="">IOS</p>
+          <p className="text-3xl text-[#756AB6] font-bold">Apple</p>
+          <p className="text-3xl text-[#756AB6] font-bold mb-4">iPhone 15</p>
+          <button className="font-bold py-2 px-4 bg-[#756AB6] rounded-xl">
+            Shop Now
           </button>
         </div>
       </div>
 
-      {/* 2nd div */}
-
-      <div className="md:w-1/2 w-full flex flex-col gap-2 relative">
-        <div className="flex-grow h-[180px] relative">
-          <img
-            src="https://images.pexels.com/photos/56904/pexels-photo-56904.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt=""
-            className="w-full h-full rounded-2xl object-cover"
-          />
-          <div className="absolute top-3 left-0 p-4 text-white mx-5 ">
-            <p className="">IOS</p>
-            <p className="text-3xl text-[#756AB6] font-bold">Apple</p>
-            <p className="text-3xl text-[#756AB6] font-bold mb-4">iPhone 15</p>
-            <button className="font-bold py-2 px-4 bg-[#756AB6] rounded-xl">
-              Shop Now
-            </button>
-          </div>
-        </div>
-
-        <div className="flex-grow h-[180px] relative">
-          <img
-            src="https://images.pexels.com/photos/56904/pexels-photo-56904.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt=""
-            className="w-full h-full rounded-2xl object-cover"
-          />
-          <div className="absolute top-3 left-0 p-4 text-white mx-5 ">
-            <p className="">Android</p>
-            <p className="text-3xl text-[#756AB6] font-bold">Google</p>
-            <p className="text-3xl text-[#756AB6] font-bold mb-4">Pixel 7a</p>
-            <button className="font-bold py-2 px-4 bg-[#FFA732] rounded-xl">
-              Shop Now
-            </button>
-          </div>
+      <div className="flex-grow h-[180px] relative">
+        <img
+          src="https://images.pexels.com/photos/56904/pexels-photo-56904.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          className="w-full h-full rounded-2xl object-cover"
+        />
+        <div className="absolute top-3 left-0 p-4 text-white mx-5 ">
+          <p className="">Android</p>
+          <p className="text-3xl text-[#756AB6] font-bold">Google</p>
+          <p className="text-3xl text-[#756AB6] font-bold mb-4">Pixel 7a</p>
+          <button className="font-bold py-2 px-4 bg-[#FFA732] rounded-xl">
+            Shop Now
+          </button>
         </div>
       </div>
     </div>
-  );
+  </div>
+  ) 
 }
 
 export default Hero1;
