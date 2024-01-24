ec24368d07d6fb343e46c36989ff88645ecbc119
Ravi Kumar <ravi14577@gmail.com>
2024-01-24 14:59:00 +0530

    flash sale section complete only img required

--- src/components/Homepage/SmartBuy.jsx
+++ src/components/Homepage/SmartBuy.jsx
@@ -30,7 +30,7 @@ const SmartBuy = () => {
   ];
 
   return (
-    <>
+    <div className="mb-5">
       <div className="bg-blue-300 font-bold p-8 flex justify-between">
         <p className="text-6xl font-serif">SMART BUY</p>
         <h1
@@ -72,16 +72,15 @@ const SmartBuy = () => {
           <p className="font-bold text-5xl">FLASH SALE</p>
           <p className="bg-purple-500 text-white mx-20">UP TO 70% OFF</p>
           {/* flipclock start */}
-          <div className="flex mt-5 text-center">
+          <div className="flex items-center justify-center mt-5">
             <div className="font-bold text-white text-center">
               <div className="text-[#8486A9]">
                 <FlipCountdown
                   hideYear
                   hideMonth
                   theme="dark"
-                  size="small"
+                  size="medium"
                   titlePosition="bottom"
-                  text="center"
                   endAt={new Date(
                     Date.now() +
                       1000 /* sec */ *
@@ -96,24 +95,50 @@ const SmartBuy = () => {
               </div>
             </div>
           </div>
+
           {/* flipclock ends */}
           <div className="text-left flex justify-between">
-            <p>IPHONE 15 PRO</p>
-            <button className="bg-orange-900 text-white px-10 py-2 rounded-2xl">
-              Shop Now
-            </button>
-            <img src="../assets/HomeImages/apple.png" alt="apple" />
+            <div className=" mx-16 mt-[150px]">
+              <p className="mb-10 font-bold text-2xl">IPHONE 15 PRO</p>
+              <button className="bg-orange-900 text-white px-10 py-2 rounded-2xl">
+                Shop Now
+              </button>
+            </div>
+
+            <img src="../assets/HomeImages/404.png" alt="apple" />
           </div>
         </div>
+        {/* flagsale coing end */}
+
+        {/* mobile app version start */}
+        <div
+          className="text-center px-5 bg-cover mt-20 h-[400px]"
+          style={{ background: "linear-gradient(to bottom, white, blue)" }}
+        >
+          <div className="text-left">
+            <p className="text-[#96E9C6] text-base font-semibold mb-5">
+              MOBILE APP VERSION
+            </p>
+            <p className="text-2xl">
+              Get Our{" "}
+              <span className="text-[#83C0C1] font-bold"> Mobile App</span> it's
+              Make Easy for your life !
+            </p>
+            <img src="" className="" alt="" />
+            <img src="" alt="" />
+
+            <p className="font-bold text-2xl mb-1">Join Membership</p>
+            <button className="w-3/4 py-2 rounded-xl mb-2 bg-gray-300 hover:bg-slate-500">
+              Prime Membership
+            </button>
+            <button className="w-3/4 py-2 rounded-xl  bg-yellow-200 hover:bg-yellow-500">
+              Supreme Membership
+            </button>
+          </div>
+        </div>
+        {/* mobile app version start */}
       </div>
-      {/* flagsale coing end */}
-
-      {/* mobile app version start */}
-
-      {/* mobile app version start */}
-
-      <div></div>
-    </>
+    </div>
   );
 };
 
